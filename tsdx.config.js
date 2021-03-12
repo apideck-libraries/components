b142c6163c7eec11fc8199735b234e3e7d27bb74
Jake Prins <jakeprins@outlook.com>
2021-03-12 13:54:32 +0100

    Update rollup config to include external dependencies in bundle

--- tsdx.config.js
+++ tsdx.config.js
@@ -2,6 +2,9 @@ const postcss = require('rollup-plugin-postcss')
 
 module.exports = {
   rollup(config, options) {
+    if (config.output.format === 'umd') {
+      delete config.external
+    }
     config.plugins.push(
       postcss({
         config: {
