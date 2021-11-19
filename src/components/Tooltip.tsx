df5737e84b15ad16df08064071a686f7b8ac00a1
Jake Prins <jakeprins@outlook.com>
2021-11-19 10:23:48 +0100

    Add darkmode support to tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -47,17 +47,20 @@ export const Tooltip = ({ children, text, alignPointerRight = false, styles, ...
       {...other}
     >
       <div
-        className="absolute z-10 flex items-center px-3 py-2 text-sm text-gray-800 whitespace-no-wrap transition-all duration-150 bg-white border border-gray-200 rounded shadow-sm"
+        className="absolute z-10 flex items-center px-3 py-2 text-sm text-gray-800 whitespace-no-wrap transition-all duration-150 bg-white border border-gray-200 rounded shadow-sm dark:text-gray-300 dark:border-gray-700 dark:bg-gray-800"
         style={{ top: '100%', opacity: 0, display: 'none', ...styles }}
         data-testid="tooltip"
         ref={ref}
       >
         {text}
         <div
-          className={classNames('absolute w-3 h-3 bg-white border-t border-l border-gray-200', {
-            'right-2': alignPointerRight,
-            'left-2': !alignPointerRight
-          })}
+          className={classNames(
+            'absolute w-3 h-3 bg-white border-t border-l border-gray-200 dark:text-gray-300 dark:border-gray-700 dark:bg-gray-800',
+            {
+              'right-2': alignPointerRight,
+              'left-2': !alignPointerRight
+            }
+          )}
           data-testid="pointer"
           style={{ top: '-7px', transform: 'rotate(45deg)' }}
         />
