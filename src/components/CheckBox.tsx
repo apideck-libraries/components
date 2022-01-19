f6481da733cd98ad4ed0430bda8c59983b90a12f
Jake Prins <jakeprins@outlook.com>
2022-01-19 20:52:59 +0100

    Fix checkboc label

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes, forwardRef } from 'react'
+import React, { Fragment, HTMLAttributes, ReactNode, forwardRef } from 'react'
 
 import classNames from 'classnames'
 import styles from '../styles/input'
@@ -20,7 +20,7 @@ export const CheckBox = forwardRef<HTMLInputElement, Props>(function CheckBox(
   ref
 ) {
   return (
-    <>
+    <Wrapper label={label} disabled={disabled} name={name}>
       <input
         className={classNames(
           'w-5 h-5 text-primary-600 border-gray-300 shadow-sm rounded-md focus:ring-primary-500 focus:border-primary-500',
@@ -42,21 +42,45 @@ export const CheckBox = forwardRef<HTMLInputElement, Props>(function CheckBox(
         data-testid={name}
         {...other}
       />
-      {label && (
-        <label
-          htmlFor={name}
-          className={classNames(
-            'inline-block ml-2 text-sm text-gray-600 dark:text-white dark:focus:text-white dark:bg-gray-800 dark:focus:bg-gray-900',
-            {
-              'cursor-not-allowed': disabled
-            }
-          )}
-        >
-          {label}
-        </label>
-      )}
-    </>
+    </Wrapper>
   )
 })
+
+const Wrapper = ({
+  children,
+  label,
+  disabled,
+  name
+}: {
+  name: string
+  label?: string
+  disabled?: boolean
+  children: ReactNode
+}) =>
+  label ? (
+    <label
+      htmlFor={name}
+      className={classNames(
+        'inline-block ml-2 text-sm text-gray-600 dark:text-white dark:focus:text-white dark:bg-gray-800 dark:focus:bg-gray-900',
+        {
+          'cursor-not-allowed': disabled
+        }
+      )}
+    >
+      {children}
+      <span
+        className={classNames(
+          'inline-block ml-2 text-sm text-gray-600 dark:text-white dark:focus:text-white dark:bg-gray-800 dark:focus:bg-gray-900',
+          {
+            'cursor-not-allowed': disabled
+          }
+        )}
+      >
+        {label}
+      </span>
+    </label>
+  ) : (
+    <Fragment>{children}</Fragment>
+  )
 
 export default CheckBox
