e6d9caa50f46e0930c74d8f70d4ee51794da09d3
Jake Prins <jakeprins@outlook.com>
2022-01-31 13:47:04 +0100

    Update tailwind, postcss, autoprefixer, and storybook

--- tailwind-config.js
+++ tailwind-config.js
@@ -1,28 +1,25 @@
 const deepMerge = require('deepmerge')
 
 const apideckComponentsConfig = {
-  purge: ['./src/**/*.tsx'],
-  darkMode: false,
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         primary: {
-          '50': '#f6f7fe',
-          '100': '#f2f3fd',
-          '200': '#e0e1fa',
-          '300': '#c9c8f4',
-          '400': '#aba4ea',
-          '500': '#9182de',
-          '600': '#775ad8',
-          '700': '#6434d5',
-          '800': '#5922b9',
-          '900': '#5a1aa8'
+          50: '#f6f7fe',
+          100: '#f2f3fd',
+          200: '#e0e1fa',
+          300: '#c9c8f4',
+          400: '#aba4ea',
+          500: '#9182de',
+          600: '#775ad8',
+          700: '#6434d5',
+          800: '#5922b9',
+          900: '#5a1aa8'
         }
       }
     }
-  },
-  variants: {
-    extend: {}
   },
   plugins: [require('@tailwindcss/forms')]
 }
@@ -35,7 +32,7 @@ const arrayMerge = (destinationArray, sourceArray) => {
 }
 
 // Takes a Tailwind configuration object and merges it with the current configuration
-const tailwindConfigWrapper = tailwindConfig => {
+const tailwindConfigWrapper = (tailwindConfig) => {
   return deepMerge(apideckComponentsConfig, tailwindConfig, { arrayMerge })
 }
 
