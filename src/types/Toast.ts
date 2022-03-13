c7f8d9f750813627663746f57dea6e78d68b1284
Jake Prins <jakeprins@outlook.com>
2022-03-13 13:44:06 +0100

    Set autoClose default to true on Toast component and change type

--- src/types/Toast.ts
+++ src/types/Toast.ts
@@ -1,6 +1,8 @@
+import { ReactNode } from 'react'
+
 export interface Toast {
   title: string
-  description?: string
+  description?: string | Element | ReactNode
   type?: 'success' | 'warning' | 'error' | 'info'
   id?: number
   autoClose?: boolean
