5b1acc1a61b958a25684edf1981d3188cdcb1740
brahimmouhamou <brahim@stravan.be>
2022-03-21 10:28:06 +0100

    Add Chip component

---
+++ src/components/Chip.tsx
@@ -0,0 +1,130 @@
+import React, { HTMLAttributes, forwardRef, ReactNode } from 'react'
+import classNames from 'classnames'
+
+export type Props = HTMLAttributes<HTMLDivElement> & {
+  className?: string
+  colorClassName?: string
+  label: string
+  size?: 'small' | 'regular' | 'large'
+  iconComponent?: ReactNode
+  iconUrl?: string
+  iconInline?: boolean
+  onClose?: VoidFunction
+}
+
+const chipSizeStyles = {
+  small: 'h-5',
+  regular: 'h-7',
+  large: 'h-9'
+}
+
+const textSizeStyles = {
+  small: 'text-xs',
+  regular: 'text-sm',
+  large: 'text-base'
+}
+
+const closeSizeStyles = {
+  small: 'mr-2',
+  regular: 'ml-2 mr-3',
+  large: 'ml-3 mr-3'
+}
+
+export const Chip = forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const {
+    className,
+    colorClassName = classNames(
+      'bg-primary-200',
+      'text-primary-900',
+      'hover:text-primary-700',
+      'dark:hover:text-primary-400'
+    ),
+    size = 'regular',
+    label,
+    iconComponent,
+    iconUrl,
+    iconInline = false,
+    onClose
+  } = props
+
+  const hasCloseBtn = !!onClose
+  const hasIcon = !!iconUrl || !!iconComponent
+
+  const labelSizeStyles = {
+    small: `${!hasIcon && 'ml-2'} ${!hasCloseBtn && 'mr-2'}`,
+    regular: `${!hasIcon && 'ml-4'} ${!hasCloseBtn && 'mr-4'}`,
+    large: `${!hasIcon && 'ml-5'} ${!hasCloseBtn && 'mr-4'}`
+  }
+
+  const iconSizeStyles = {
+    small: `mr-1 ${iconInline ? 'pl-0.5 py-0.5' : 'px-0 py-0'}`,
+    regular: `mr-2 ${iconInline ? 'pl-1 py-1' : 'px-0 py-0'}`,
+    large: `mr-3 ${iconInline ? 'pl-1 py-1' : 'px-0 py-0'}`
+  }
+
+  const chipStyle = chipSizeStyles[size]
+  const labelStyle = `${textSizeStyles[size]} ${labelSizeStyles[size]}`
+  const closeStyle = `ml-2 ${closeSizeStyles[size]}`
+  const iconStyle = hasIcon && iconSizeStyles[size]
+
+  return (
+    <div
+      data-testid="chip"
+      ref={ref}
+      className={classNames(
+        'inline-flex items-center rounded-full transition-opacity ease-in-out',
+        chipStyle,
+        colorClassName,
+        className
+      )}
+    >
+      {hasIcon && (
+        <>
+          {iconComponent ? (
+            <div
+              className={classNames('inline-flex items-center h-full mr-2', iconStyle)}
+              data-testid="chip-image-component"
+            >
+              {iconComponent}
+            </div>
+          ) : (
+            iconUrl && (
+              <div
+                className={classNames('inline-flex items-center h-full mr-2', iconStyle)}
+                data-testid="chip-image-url"
+              >
+                <img src={iconUrl} alt="icon" className={classNames('h-full rounded-full')} />
+              </div>
+            )
+          )}
+        </>
+      )}
+      <span data-testid="chip-label" className={classNames('text-xs font-semibold', labelStyle)}>
+        {label}
+      </span>
+      {onClose && (
+        <button
+          data-testid="chip-close-btn"
+          className={classNames('bg-transparent hover focus:outline-none', closeStyle)}
+          onClick={onClose}
+        >
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="times"
+            className="w-2"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 352 512"
+          >
+            <path
+              fill="currentColor"
+              d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+            ></path>
+          </svg>
+        </button>
+      )}
+    </div>
+  )
+})
---
+++ tests/Chip.test.tsx
@@ -0,0 +1,132 @@
+import '@testing-library/jest-dom/extend-expect'
+
+import * as React from 'react'
+
+import { fireEvent, render } from '@testing-library/react'
+
+import { Default as Chip } from '../stories/Chip.stories'
+
+describe('Chip', () => {
+  it('should render the Chip without crashing', () => {
+    const { getByText } = render(<Chip label="test" />)
+    expect(getByText('test')).toBeInTheDocument()
+  })
+
+  it('should render the Chip with the provided text prop', () => {
+    const { getByTestId } = render(<Chip label="cool chip" />)
+    expect(getByTestId('chip-label')).toHaveTextContent('cool chip')
+  })
+
+  it('should contain base classes', () => {
+    const expected = 'inline-flex items-center rounded-full transition-opacity ease-in-out'
+    const { getByTestId } = render(<Chip label="test" />)
+    const chipElement = getByTestId('chip')
+
+    expect(chipElement.getAttribute('class')).toContain(expected)
+  })
+
+  describe('Sizes', () => {
+    it('should render a small chip', () => {
+      const { getByTestId } = render(<Chip label="test" size="small" />)
+      const chipElement = getByTestId('chip-label')
+
+      expect(chipElement.classList.contains('text-xs')).toBe(true)
+    })
+
+    it('should render a regular chip', () => {
+      const { getByTestId } = render(<Chip label="test" size="regular" />)
+      const chipElement = getByTestId('chip-label')
+
+      expect(chipElement.classList.contains('text-sm')).toBe(true)
+    })
+
+    it('should render a large chip', () => {
+      const { getByTestId } = render(<Chip label="test" size="large" />)
+      const chipElement = getByTestId('chip-label')
+
+      expect(chipElement.classList.contains('text-base')).toBe(true)
+    })
+  })
+
+  describe('With image url', () => {
+    it('should render an image', () => {
+      const { getByTestId } = render(
+        <Chip label="test" iconUrl="https://picsum.photos/50/50" iconInline />
+      )
+
+      const imageWrapper = getByTestId('chip-image-url')
+      const expected = 'pl-1 py-1'
+
+      expect(imageWrapper.getAttribute('class')).toContain(expected)
+    })
+
+    it('should render an inline image', () => {
+      const ImageComponent = () => <div>dummy img component</div>
+      const { getByTestId } = render(
+        <Chip label="test" iconComponent={<ImageComponent />} iconInline />
+      )
+      const imageComponentWrapper = getByTestId('chip-image-component')
+      const expected = 'pl-1 py-1'
+
+      expect(imageComponentWrapper.getAttribute('class')).toContain(expected)
+    })
+  })
+
+  describe('With image component', () => {
+    it('should render an image', () => {
+      const ImageComponent = () => <div>dummy img component</div>
+      const { getByTestId } = render(<Chip label="test" iconComponent={<ImageComponent />} />)
+
+      expect(getByTestId('chip-image-component')).toBeInTheDocument()
+    })
+
+    it('should render an inline image', () => {
+      const ImageComponent = () => <div>dummy img component</div>
+      const { getByTestId } = render(
+        <Chip label="test" iconComponent={<ImageComponent />} iconInline />
+      )
+      const imageComponentWrapper = getByTestId('chip-image-component')
+      const expected = 'pl-1 py-1'
+
+      expect(imageComponentWrapper.getAttribute('class')).toContain(expected)
+    })
+  })
+
+  describe('With close button', () => {
+    it('should render a close btn', () => {
+      const mockClick = jest.fn()
+      const { getByTestId } = render(<Chip label="test" onClose={mockClick} />)
+
+      expect(getByTestId('chip-close-btn')).toBeInTheDocument()
+    })
+
+    it('should call the onClose method when a user clicks on the close button', () => {
+      const mockClick = jest.fn()
+      const { getByTestId } = render(<Chip label="test" onClose={mockClick} />)
+      const closeBtn = getByTestId('chip-close-btn')
+      fireEvent.click(closeBtn)
+
+      expect(closeBtn).not.toBeDisabled()
+      expect(mockClick).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('With classes', () => {
+    it('should extend classes', () => {
+      const { getByTestId } = render(<Chip label="test" className="test" />)
+      const chipElement = getByTestId('chip')
+
+      expect(chipElement.classList.contains('test')).toBe(true)
+    })
+  })
+
+  describe('With color classes', () => {
+    it('should render the color classes', () => {
+      const colorClasses = 'bg-red-300 text-green-300'
+      const { getByTestId } = render(<Chip label="test" colorClassName={colorClasses} />)
+
+      const chipElement = getByTestId('chip')
+      expect(chipElement.getAttribute('class')).toContain(colorClasses)
+    })
+  })
+})
