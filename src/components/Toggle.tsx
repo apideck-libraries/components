8bc60c8578e2183ae22aa67eb703f387a2d0dbf7
Jake Prins <jakeprins@outlook.com>
2022-03-23 10:53:20 +0100

    Change loading state of Toggle

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,6 +1,5 @@
-import React, { ButtonHTMLAttributes, forwardRef } from 'react'
-
 import classNames from 'classnames'
+import React, { ButtonHTMLAttributes, forwardRef } from 'react'
 
 export interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   onToggle: () => void
@@ -36,10 +35,34 @@ export const Toggle = forwardRef<HTMLButtonElement, Props>(function Toggle(
           {
             'translate-x-5': isEnabled,
             'translate-x-0': !isEnabled,
-            'animate-ping duration-200': isLoading
+            'p-1': isLoading
           }
         )}
-      ></span>
+      >
+        <svg
+          data-testid="loading-svg"
+          className={classNames('text-gray-400 animate-spin', {
+            hidden: !isLoading
+          })}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </span>
     </button>
   )
 })
