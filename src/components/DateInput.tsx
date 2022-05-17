28244cbc480a2973644e41abc86d38127cb43646
Jake Prins <jakeprins@outlook.com>
2022-05-17 11:41:07 +0200

    Add onClear prop to dateinput

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -16,6 +16,7 @@ export interface Props {
   minDate?: string | number | Date
   maxDate?: string | number | Date
   isRange?: boolean
+  onClear?: () => void
 }
 
 export const DateInput: React.FC<Props> = ({
@@ -30,6 +31,7 @@ export const DateInput: React.FC<Props> = ({
   minDate,
   maxDate,
   isRange = false,
+  onClear,
   ...other
 }) => {
   const inputRef = useRef<HTMLInputElement>(null)
@@ -244,9 +246,12 @@ export const DateInput: React.FC<Props> = ({
         disabled={disabled}
         {...other}
       />
+
       <div className="absolute top-0 right-0 z-10 px-3 py-2">
         <svg
-          className="w-5 h-5 text-gray-400"
+          className={classNames('w-5 h-5 text-gray-400', {
+            'opacity-0': datePickerValue && onClear
+          })}
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -258,6 +263,27 @@ export const DateInput: React.FC<Props> = ({
             d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
           />
         </svg>
+        {datePickerValue && onClear && (
+          <button
+            className="absolute hover:bg-gray-100 rounded-full flex items-center justify-center p-1 top-2 right-2"
+            style={{ top: 7 }}
+            onClick={() => {
+              setDatePickerValue('')
+              onClear()
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        )}
       </div>
       <div
         data-testid="datePicker"
