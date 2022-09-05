ae1f2b34077c6b5fbebc20edf4de59499fe3873b
Jake Prins <jakeprins@outlook.com>
2022-09-05 16:38:37 +0200

    Fix tests

--- tests/Toggle.test.tsx
+++ tests/Toggle.test.tsx
@@ -1,17 +1,20 @@
 import '@testing-library/jest-dom/extend-expect'
-import { fireEvent, render } from '@testing-library/react'
+
 import * as React from 'react'
+
+import { fireEvent, render } from '@testing-library/react'
+
 import { Default as Toggle } from '../stories/Toggle.stories'
 
 describe('Toggle', () => {
   it('should render the component', async () => {
     const { getByRole } = render(<Toggle isEnabled={false} onToggle={() => {}} />)
-    expect(getByRole('button')).toBeInTheDocument()
+    expect(getByRole('switch')).toBeInTheDocument()
   })
 
   it('should show enabled state', async () => {
     const { getByRole } = render(<Toggle isEnabled={true} onToggle={() => {}} />)
-    const toggle = getByRole('button')
+    const toggle = getByRole('switch')
 
     expect(toggle.classList.contains('bg-primary-600')).toBe(true)
     expect(toggle.classList.contains('bg-gray-200')).toBe(false)
@@ -38,7 +41,7 @@ describe('Toggle', () => {
   it('should call the provided function on toggle', () => {
     const MockFunction = jest.fn()
     const { getByRole } = render(<Toggle isEnabled={false} onToggle={() => MockFunction()} />)
-    const toggle = getByRole('button')
+    const toggle = getByRole('switch')
 
     fireEvent.click(toggle)
     expect(MockFunction).toHaveBeenCalledTimes(1)
@@ -46,7 +49,7 @@ describe('Toggle', () => {
 
   it('should extend classes', () => {
     const { getByRole } = render(<Toggle isEnabled={false} onToggle={() => {}} className="test" />)
-    const toggle = getByRole('button')
+    const toggle = getByRole('switch')
     expect(toggle.classList.contains('test')).toBe(true)
   })
 
@@ -54,7 +57,7 @@ describe('Toggle', () => {
     const { getByRole } = render(
       <Toggle isEnabled={false} onToggle={() => {}} style={{ maxWidth: 200 }} />
     )
-    const toggle = getByRole('button')
+    const toggle = getByRole('switch')
     const style = window.getComputedStyle(toggle)
     expect(style.maxWidth).toBe('200px')
   })
